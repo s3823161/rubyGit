324af69cb2bc16eb8fe05a74450130fd31af6ebc
nohkiee <s3823161@student.rmit.edu.au>
2021-03-17 16:47:45 +1100

    Added operator testing ruby file

---
+++ operatorTesting.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/ruby
+
+$var1 = 1
+var2 = 2
+Const1 = 111
+
+def method1
+	puts "Method1"
+end
+
+class Teeny
+	Const2 = 222
+	def method2
+		puts "Method2"
+	end
+
+	def self.method3
+		puts "Method3"
+	end
+end
+
+class Tiny
+	const3 = 333
+	def method4
+	end
+
+	def method5
+	end
+end
+
+class Test < Tiny
+	@@var3 = 3
+	@var4
+
+	def initialize
+		@var4 = 4
+	end
+
+	def method5
+		_var5 = 5
+
+		puts "#{defined? $var1 			or "UNDEFINED"}"
+		puts "#{defined? var2 			or "UNDEFINED"}"
+		puts "#{defined? @@var3 		or "UNDEFINED"}"
+		puts "#{defined? @var4 			or "UNDEFINED"}"
+		puts "#{defined? _var5 			or "UNDEFINED"}"
+		puts "#{defined? var6 			or "UNDEFINED"}"
+
+		puts "#{defined? Const1 		or "UNDEFINED"}"
+		puts "#{defined? Const2 		or "UNDEFINED"}"
+		puts "#{defined? Const3			or "UNDEFINED"}"
+
+		puts "#{defined? method1 		or "UNDEFINED"}"
+		puts "#{defined? method2 		or "UNDEFINED"}"
+		puts "#{defined? Teeny.method3 	or "UNDEFINED"}"
+
+		puts "#{defined? method4 		or "UNDEFINED"}"
+		puts "#{defined? super	 		or "UNDEFINED"}"
+		
+		puts "#{defined? Teeny	 		or "UNDEFINED"}"
+	end
+end
+
+Teeny.new
+Test.new.method5
--- tuteWeek2/partOne.rb
+++ tuteWeek2/partOne.rb
@@ -1,1 +1,30 @@
-# This is the first file, ayeee
+#!/usr/bin/env ruby
+
+$tax_rate = 0.1 # 10%
+
+class Car
+	#Attr_reader allows access to variable (a getter named 'type')
+	attr_reader		:type
+	#attr_accessor defines methods value and value= (A Getter and a setter).
+	attr_accessor	:value
+
+	#Constructor
+	def initialize(type, value)
+		@type = type;
+		@value = value * (1+$tax_rate);
+	end
+
+	def to_s
+		"Car model: %s, price: %d" % [type, value];
+	end
+end
+
+
+car1 = Car.new("Nissan", 15000);
+car2 = Car.new("BMW", 45000);
+
+puts car1
+puts car2
+
+car1.value = 60000;
+puts car1
